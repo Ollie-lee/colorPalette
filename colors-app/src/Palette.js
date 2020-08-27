ac6f82dc0a17b22f60dcb7a3254bc22c5a1d8d6c
Ollie Lee <myself.ollie.lee@gmail.com>
2020-08-27 15:20:08 +1000

    style slider

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox'
-import './Palette.css'
-export class Palette extends Component {
+import 'rc-slider/assets/index.css';
 
+import './Palette.css'
+import Slider, { Range } from 'rc-slider';
+export class Palette extends Component {
+    state = { level: 500 }
+    changeLevel = (newLevel) => {
+        this.setState({ level: newLevel })
+    }
     render() {
-        const colorBoxes = this.props.palette.colors[600].map(color =>
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[level].map(color =>
             (<ColorBox background={color.hex}
                 name={color.name} />))
         return (
             <div className='Palette'>
+                <div className='slider'>
+                    <Slider defaultValue={this.state.level}
+                        min={100}
+                        max={900}
+                        step={100}
+                        onAfterChange={this.changeLevel} />
+                </div>
+
                 {/* todo: navbar */}
                 <div className='Palette-colors'>
                     {/* bunch of color boxes */}
