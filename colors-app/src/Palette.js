4a8d7f184838328f3e4e6d11a29203c2c897a415
Ollie Lee <myself.ollie.lee@gmail.com>
2020-08-29 16:14:57 +1000

    add footer

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -13,11 +13,12 @@ export class Palette extends Component {
     }
 
     render() {
-        const { colors } = this.props.palette;
+        const { colors, paletteName, emoji } = this.props.palette;
         const { level, format } = this.state;
         const colorBoxes = colors[level].map(color =>
             (<ColorBox background={color[format]}
-                name={color[format]} />))
+                name={color[format]}
+                key={color.id} />))
         return (
             <div className='Palette'>
                 <Navbar level={level}
@@ -30,6 +31,10 @@ export class Palette extends Component {
                     {colorBoxes}
                 </div>
                 {/* footer eventually */}
+                <footer className='Palette-footer'>
+                    {paletteName}
+                    <span className='emoji'>{emoji}</span>
+                </footer>
             </div>
         )
     }
