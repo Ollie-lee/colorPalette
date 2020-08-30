e3eb1a7b990ffd1d444823a3adf89f025aec66a4
Ollie Lee <myself.ollie.lee@gmail.com>
2020-08-30 16:20:46 +1000

    install matreial UI style

---
+++ colors-app/src/Minipalette.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Minipalette() {
+  return (
+    <div>
+      <h1>Mini palette</h1>
+    </div>
+  )
+}
--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Minipalette from './Minipalette'
 
 export default class PaletteList extends Component {
   render() {
@@ -10,7 +11,7 @@ export default class PaletteList extends Component {
         <h1>React colors</h1>
         {palettes.map(palette =>
           <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+            <Link to={`/palette/${palette.id}`}><Minipalette /></Link>
           </p>
         )}
       </div>
