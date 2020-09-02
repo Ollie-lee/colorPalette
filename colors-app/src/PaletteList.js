ffa8ad8cbea6eef92902f5ed3535729620f5377c
Ollie Lee <myself.ollie.lee@gmail.com>
2020-09-02 13:40:25 +1000

    refactor Jss

--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -3,37 +3,9 @@ import { Link } from 'react-router-dom'
 import Minipalette from './Minipalette'
 import { withStyles } from '@material-ui/core/styles';
 import Palette from './Palette';
+import styles from './styles/PaletteListStyle'
 
-const styles = {
-  root: {
-    backgroundColor: "blue",
-    height: '100vh',
-    display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-  },
-  container: {
-    width: '50%',
-    display: 'flex',
-    alignItems: 'flex-start',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
 
-  },
-  nav: {
-    display: 'flex',
-    width: '100%',
-    justifyContent: 'space-between',
-    color: 'white'
-  },
-  palettes: {
-    boxSizing: 'border-box',
-    width: '100%',
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3,30%)',
-    gridGap: '5%'
-  }
-}
 
 class PaletteList extends Component {
 
