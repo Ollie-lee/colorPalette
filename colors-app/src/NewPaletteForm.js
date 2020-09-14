4fba2c90d92d7c02f64ff1c70b7c607be7a1abd1
Ollie Lee <myself.ollie.lee@gmail.com>
2020-09-14 17:09:00 +1000

    fix drag bug

--- colors-app/src/NewPaletteForm.js
+++ colors-app/src/NewPaletteForm.js
@@ -244,6 +244,7 @@ export default function NewPaletteForm(props) {
           axis='xy'
           onSortEnd={onSortEnd}
           className={classes.DraggableColorList}
+          distance={2}
         />
 
 
